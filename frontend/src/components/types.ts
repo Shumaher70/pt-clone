1a60d50700adcd7d0cb7a105fb6fe654ad8c01f4
Oleg Vrabiy <olegvraby@gmail.com>
2025-03-12 18:33:20 +0200

    create:src/components/image.tsx:

---
+++ frontend/src/components/types.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export interface IImageProps {
+  path: string;
+  alt?: string;
+  width?: number;
+  height?: number;
+  className?: string;
+  [key: string]: any;
+}
