289546bc67730a20f6be368383b4874db34d0b2a
Oleg Vrabiy <olegvraby@gmail.com>
2025-03-12 18:34:30 +0200

    update:widgets/constants.ts: optimizedHeight

--- frontend/src/widgets/constants.ts
+++ frontend/src/widgets/constants.ts
@@ -2,6 +2,8 @@ import { TopBar } from './top-bar/top-bar';
 import { LeftBar } from './left-bar/left-bar';
 
 import { Gallery } from './gallery/gallery';
-import { GalleryItems } from './gallery/components/gallery-item';
 
-export { TopBar, Gallery, GalleryItems, LeftBar };
+import { GalleryItem } from './gallery/components/gallery-item/gallery-item';
+import { optimizedHeight } from './gallery/components/gallery-item/utils/optimized-height';
+
+export { TopBar, Gallery, GalleryItem, LeftBar, optimizedHeight };
