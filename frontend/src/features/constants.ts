1e506162e68ed470b2ba5d7eabbe39a7db01ff8e
Oleg Vrabiy <olegvraby@gmail.com>
2025-03-18 17:53:21 +0200

    update:features/constants.ts:Auth, SignInForm, SIgnUpForm have been added

--- frontend/src/features/constants.ts
+++ frontend/src/features/constants.ts
@@ -1,16 +1,22 @@
+import { Auth } from './auth/auth';
 import { Profile } from './profile/profile';
 import { Comments } from './post/comments/comments';
 import { PostCreate } from './post-create/post-create';
+import { SignUpForm } from './auth/sign-up/sign-up-form';
+import { SignInForm } from './auth/sigh-in/sign-in-form';
 import { PostForm } from './post-create/components/post-form/post-form';
 import { PostInteractions } from './post/post-interactions/post-interactions';
 import { PostPublish } from './post-create/components/post-publish/post-publish';
 import { PostUploading } from './post-create/components/post-uploading/post-uploading';
 
 export {
-  Comments,
+  Auth,
   Profile,
+  Comments,
   PostForm,
   PostCreate,
+  SignInForm,
+  SignUpForm,
   PostPublish,
   PostUploading,
   PostInteractions,
