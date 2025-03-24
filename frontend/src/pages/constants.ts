8644641690a41661d984b57c24dd4c69853b2bc2
Oleg Vrabiy <olegvraby@gmail.com>
2025-03-24 18:43:39 +0200

    update:pages/constants.ts: ErrorPage, LoadingPage have been added

--- frontend/src/pages/constants.ts
+++ frontend/src/pages/constants.ts
@@ -10,24 +10,30 @@ import { homeRoutes } from './home-page/routes';
 import { postRoutes } from './post-page/routes';
 import { createRoutes } from './create-page/routes';
 import { searchRoutes } from './search-page/routes';
+import { errorRoutes } from './layouts/error/routes';
 import { profileRoutes } from './profile-page/routes';
 
+import ErrorPage from './layouts/error/error-page';
 import LayoutApp from './layouts/layout-app/layout-app';
+import LoadingPage from './layouts/loading/loading-page';
 
 export default {
   AuthPage,
   HomePage,
   PostPage,
+  ErrorPage,
   LayoutApp,
   CreatePage,
   SearchPage,
   ProfilePage,
+  LoadingPage,
 };
 
 export {
   authRoutes,
   homeRoutes,
   postRoutes,
+  errorRoutes,
   createRoutes,
   searchRoutes,
   profileRoutes,
