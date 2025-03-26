082461d34b5059b035b1540012d3d176e9063447
Oleg Vrabiy <olegvraby@gmail.com>
2025-03-26 16:47:00 +0200

    update:search.tsx: useValidateSearchInput has been added

--- frontend/src/widgets/top-bar/components/search/search.tsx
+++ frontend/src/widgets/top-bar/components/search/search.tsx
@@ -1,10 +1,13 @@
+import { useValidateSearchInput } from './hooks/use-validate-search-input';
 import * as S from './search.styled';
 
 export const Search = () => {
+  const { handleSubmit } = useValidateSearchInput();
+
   return (
-    <S.Wrapper>
+    <S.Wrapper onSubmit={handleSubmit}>
       <S.Img path="/general/search.svg" alt="search icon" />
-      <S.Input type="test" placeholder="Search" />
+      <S.Input type="text" name="search" placeholder="Search" />
     </S.Wrapper>
   );
 };
